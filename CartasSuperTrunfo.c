203017f097e94fc4d2e205549f03e6c97bc0d5c2
WashingtonMelo <fcowashington@gmail.com>
2025-01-04 20:33:28 +0000

    Inclusão do nome do aluno

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,10 +4,12 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Teste Washington
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    
+
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     // Cadastro das Cartas:
